82797e84a6b6a2bfb4e535ab05a7caba693ffbca
mis1nput <reddragonz2005@protonmail.com>
2025-02-26 11:06:29 -0500

    IDK what to do

--- src/paswrdmngr/views/Views.java
+++ src/paswrdmngr/views/Views.java
@@ -1,11 +1,23 @@
 package paswrdmngr.views;
 
-import java.util.List;
-
 public class Views {
 
     public void show() {
+        clearScreen();
+        System.out.println("Welcome to Password Manager");
+    }
 
+    private void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    public void showMenu() {
+        System.out.println("1. Add new password");
+        System.out.println("2. Show all passwords");
+        System.out.println("3. Search password");
+        System.out.println("4. Delete password");
+        System.out.println("5. Exit");
     }
 
 
