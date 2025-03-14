d877a59e3cb98f17000c07054aa7e498838a0d2c
mis1nput <161495409+mis1nput@users.noreply.github.com>
2025-03-13 19:13:35 -0600

    Delete src/paswrdmngr directory

--- src/paswrdmngr/Main.java
+++
@@ -1,9 +0,0 @@
-package paswrdmngr;
-
-import paswrdmngr.controller.Controller;
-
-public class Main {
-    public static void main(String[] args) {
-        new Controller().run();
-        };
-    }
